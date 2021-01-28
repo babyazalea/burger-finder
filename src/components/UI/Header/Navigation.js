b2ff2818740ee0f61fbe3d72de5c87709724c5b0
babyazalea <usun12@hotmail.com>
2021-01-29 01:14:23 +0900

    added reset css, nav-links

--- src/components/UI/Header/Navigation.js
+++ src/components/UI/Header/Navigation.js
@@ -1,12 +1,21 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
+
+import { Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 
 import "./Navigation.scss";
 
 const Navigation = (props) => {
   return (
     <Navbar expand="lg">
-      <Navbar.Brand href="/">Burger Finder</Navbar.Brand>
+      <Navbar.Brand href="/">햄버거를 찾아서</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/burger-maker">버거만들기</Nav.Link>
+          <Nav.Link href="/credits">크레딧</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
