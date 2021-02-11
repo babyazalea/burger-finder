d922f77ece9c608910a7dc7ab954c4c0a52cfccd
babyazalea <usun12@hotmail.com>
2021-02-11 22:18:24 +0900

    setup to burger-assets

--- src/components/BurgerMaker/Burger/Burger.js
+++ src/components/BurgerMaker/Burger/Burger.js
@@ -3,9 +3,25 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import "./Burger.scss";
 
+import lidBunImage from "../../../assets/images/lid-bun-final.png";
+import pattyImage from "../../../assets/images/patty-final.png";
+import lettuceImage from "../../../assets/images/lettuce-final.png";
+import cheeseImage from "../../../assets/images/cheese-final.png";
+
 const Burger = (props) => (
   <Container className="burger">
-    <div className="lid-bun"></div>
+    <div className="lid-bun">
+      <img src={lidBunImage} alt="lid-bun" />
+    </div>
+    <div className="patty">
+      <img src={pattyImage} alt="patty" />
+    </div>
+    <div className="lettuce">
+      <img src={lettuceImage} alt="lettuce" />
+    </div>
+    <div className="cheese">
+      <img src={cheeseImage} alt="cheese" />
+    </div>
   </Container>
 );
 
