8a4e9933a4b94108aa7ff06c8cbdcbd316180ac0
babyazalea <usun12@hotmail.com>
2021-03-22 17:43:15 +0900

    modified BaseCard className

--- src/components/UI/BaseCard/BaseCard.js
+++ src/components/UI/BaseCard/BaseCard.js
@@ -2,15 +2,10 @@ import React from "react";
 
 import "./BaseCard.scss";
 
-const BaseCard = (props) => {
-  let className;
-  if (props.otherClassName) {
-    className = "base-card " + props.otherClassName;
-  } else {
-    className = "base-card";
-  }
-
-  return <div className={className}>{props.children}</div>;
-};
+const BaseCard = (props) => (
+  <div className={`base-card ${props.otherClassName || ""}`}>
+    {props.children}
+  </div>
+);
 
 export default BaseCard;
