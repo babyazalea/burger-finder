cd98970f6efcfe37e169f5f04f29fecd66486410
babyazalea <usun12@hotmail.com>
2021-03-24 01:46:47 +0900

    css styling for burger-analyzer

--- src/components/Burger/BurgerAnalyzer/AnalysisResults/AnalysisResults.js
+++ src/components/Burger/BurgerAnalyzer/AnalysisResults/AnalysisResults.js
@@ -5,31 +5,30 @@ import BaseCard from "../../../UI/BaseCard/BaseCard";
 import "./AnalysisResults.scss";
 
 const AnalysisResults = (props) => {
-  let results;
+  let className = "burger-ranking-item";
+  const widthAndHeight = 150 - (props.index + 1) * 10;
+  const fontSize = -props.index + 3 + "rem";
 
   if (props.score === 100) {
-    results = (
-      <li className="burger-ranking-item">
-        <BaseCard otherClassName="perfect-match-burger">
-          <span className="burger-ranking-rank">{props.index + 1}</span>
-          <span className="burger-ranking-name">{props.name}</span>
-          <span className="burger-ranking-score">{props.score}</span>
-        </BaseCard>
-      </li>
-    );
-  } else {
-    results = (
-      <li className="burger-ranking-item">
-        <BaseCard>
-          <span className="burger-ranking-rank">{props.index + 1}</span>
-          <span className="burger-ranking-name">{props.name}</span>
-          <span className="burger-ranking-score">{props.score}</span>
-        </BaseCard>
-      </li>
-    );
+    className += " perfect-match-burger";
   }
 
-  return results;
+  return (
+    <li
+      className={className}
+      style={{
+        width: widthAndHeight + "%",
+        height: widthAndHeight + "%",
+        fontSize: fontSize,
+      }}
+    >
+      <BaseCard>
+        <a href="#">
+          <span className="burger-ranking-name">{props.name}</span>
+        </a>
+      </BaseCard>
+    </li>
+  );
 };
 
 export default AnalysisResults;
--- src/components/UI/BaseCard/BaseCard.js
+++ src/components/UI/BaseCard/BaseCard.js
@@ -2,10 +2,6 @@ import React from "react";
 
 import "./BaseCard.scss";
 
-const BaseCard = (props) => (
-  <div className={`base-card ${props.otherClassName || ""}`}>
-    {props.children}
-  </div>
-);
+const BaseCard = (props) => <div className="base-card">{props.children}</div>;
 
 export default BaseCard;
