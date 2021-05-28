4f0ccc2fe9e74cac828fa99353122739e21e5677
babyazalea <usun12@hotmail.com>
2021-05-29 04:14:54 +0900

    modified importing name

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 
 import { AuthContext } from "../../../context/auth-context";
 
-import { Modal as BootStrapModal, Button } from "react-bootstrap";
+import { Modal as BootstrapModal, Button } from "react-bootstrap";
 
 const Modal = (props) => {
   const [show, setShow] = useState(false);
@@ -20,24 +20,24 @@ const Modal = (props) => {
   };
 
   return (
-    <BootStrapModal show={show} onHide={handleClose}>
-      <BootStrapModal.Header closeButton>
-        <BootStrapModal.Title>에러!</BootStrapModal.Title>
-      </BootStrapModal.Header>
-      <BootStrapModal.Body>
+    <BootstrapModal show={show} onHide={handleClose}>
+      <BootstrapModal.Header closeButton>
+        <BootstrapModal.Title>에러!</BootstrapModal.Title>
+      </BootstrapModal.Header>
+      <BootstrapModal.Body>
         {props.error
           ? props.error
           : "알 수 없는 에러가 발생했습니다. 다시 시도해보세요."}
-      </BootStrapModal.Body>
-      <BootStrapModal.Footer>
+      </BootstrapModal.Body>
+      <BootstrapModal.Footer>
         <Button variant="secondary" onClick={handleClose}>
           Close
         </Button>
         {/* <Button variant="primary" onClick={handleClose}>
           Save Changes
         </Button> */}
-      </BootStrapModal.Footer>
-    </BootStrapModal>
+      </BootstrapModal.Footer>
+    </BootstrapModal>
   );
 };
 
