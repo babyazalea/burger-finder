9df8d144eea537e1b663441febb67512becb2137
babyazalea <usun12@hotmail.com>
2021-05-29 04:16:53 +0900

    added initial value

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -3,10 +3,17 @@ import { createContext } from "react";
 export const AuthContext = createContext({
   isLoading: false,
   isLoggedIn: false,
+  error: null,
+  token: null,
   userId: null,
   userName: null,
+  sendedVerification: false,
   isVerified: false,
+  initializeError: () => {},
   authWithEmailAndPassword: () => {},
   signInToFirebase: () => {},
+  emailVerification: () => {},
+  updateProfile: () => {},
+  sendPasswordReset: () => {},
   logout: () => {},
 });
